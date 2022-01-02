151f142d09b07b092ac20d8334876f0431f814b4
Syakir  Zahari <syakirzahari97@gmail.com>
2022-01-03 00:24:19 +0800

    add dashboard menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,3 +1,10 @@
+<li class="nav-item">
+    <a class="nav-link" href="{{ route('home') }}">
+        <i class="nav-icon icon-home"></i>
+        <span>Dashboard</span>
+    </a>
+</li>
+
 <li class="nav-item {{ Request::is('venues*') ? 'active' : '' }}">
     <a class="nav-link" href="{{ route('venues.index') }}">
         <i class="nav-icon icon-cursor"></i>
