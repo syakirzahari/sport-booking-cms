d7a63bb43b80feb6ea6d76f949767486b00a328f
Syakir  Zahari <syakirzahari97@gmail.com>
2022-01-02 20:42:20 +0800

    update plugin

--- config/filesystems.php
+++ config/filesystems.php
@@ -52,6 +52,11 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
         ],
 
+        'media' => [
+            'driver' => 'local',
+            'root'   => public_path('media'),
+        ],
+
     ],
 
     /*
