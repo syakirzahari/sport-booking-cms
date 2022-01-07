a60e396f809eae0505d4e10d91d2c6cea737b9a7
Syakir  Zahari <syakirzahari97@gmail.com>
2022-01-07 12:16:43 +0800

    update route

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('', fn() => redirect(route('login')));
 
-// Auth::routes();
-Auth::routes(['register' => false]);
+Auth::routes();
+// Auth::routes(['register' => false]);
 
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
