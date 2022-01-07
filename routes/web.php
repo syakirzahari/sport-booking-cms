20035632a629e3579ff318b49974343c0a545599
Syakir  Zahari <syakirzahari97@gmail.com>
2022-01-07 12:15:11 +0800

    update route

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('', fn() => redirect(route('login')));
 
 // Auth::routes();
 Auth::routes(['register' => false]);
