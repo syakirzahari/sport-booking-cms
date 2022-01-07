cd2e26a39565b5f61dba268a460a653cf3e32db6
Syakir  Zahari <syakirzahari97@gmail.com>
2022-01-08 02:54:42 +0800

    update football api

--- app/Http/Controllers/API/FootballVenueAPIController.php
+++ app/Http/Controllers/API/FootballVenueAPIController.php
@@ -20,7 +20,7 @@ class FootballVenueAPIController extends BaseController
         $data = [];
 
         $venues = Venue::select('*')
-                    ->join('sport_venues', 'sport_venues.venue_id', '=', 'venues.id')
+                    ->leftJoin('sport_venues', 'sport_venues.venue_id', '=', 'venues.id')
                     ->where('sport_venues.sport_id', 1)
                     ->get();
     
