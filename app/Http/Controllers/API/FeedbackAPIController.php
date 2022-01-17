3e258a2f0e1e609cac89cfee435e6b6226fa3b7b
Syakir Zahari <syakirzahari97@gmail.com>
2022-01-17 10:59:40 +0800

    update api feedback

---
+++ app/Http/Controllers/API/FeedbackAPIController.php
@@ -0,0 +1,25 @@
+<?php
+   
+namespace App\Http\Controllers\API\Auth;
+   
+use Illuminate\Http\Request;
+use App\Models\Feedback;
+use App\Http\Controllers\API\BaseController as BaseController;
+   
+class FeedbackAPIController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        $input['created_by'] = auth()->user()->id;
+
+        $feedback = Feedback::create($input);
+
+        return $this->sendResponse($feedback->toArray(), 'success', 'User password changed successfully.');
+    }
+}
