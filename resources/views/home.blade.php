b8e8af9c9eb2e742e2bbd2d3d15f29939d5d493e
Syakir  Zahari <syakirzahari97@gmail.com>
2022-02-27 22:24:48 +0800

    edit template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,44 +3,45 @@
 @section('content')
   <div class="container-fluid">
         <div class="animated fadeIn">
-             <div class="row">
-                <div class="col-sm-6 col-lg-3">
-                    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-                        <div class="card-header">Header</div>
-                        <div class="card-body">
-                          <h5 class="card-title">Primary card title</h5>
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                      </div>
+          <div class="row">
+            <div class="col-sm-3">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">It's a broader card with text below as a natural lead-in to extra content. This content is a little longer.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
-                <div class="col-sm-6 col-lg-3">
-                    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-                        <div class="card-header">Header</div>
-                        <div class="card-body">
-                          <h5 class="card-title">Primary card title</h5>
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                      </div>
-                </div>
-                <div class="col-sm-6 col-lg-3">
-                    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-                        <div class="card-header">Header</div>
-                        <div class="card-body">
-                          <h5 class="card-title">Primary card title</h5>
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                      </div>
-                </div>
-                <div class="col-sm-6 col-lg-3">
-                    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-                        <div class="card-header">Header</div>
-                        <div class="card-body">
-                          <h5 class="card-title">Primary card title</h5>
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                      </div>
-                </div>
+              </div>
             </div>
+            <div class="col-sm-3">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">It's a broader card with text below as a natural lead-in to extra content. This content is a little longer.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-3">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">It's a broader card with text below as a natural lead-in to extra content. This content is a little longer.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-3">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">It's a broader card with text below as a natural lead-in to extra content. This content is a little longer.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+          </div>
+            
         </div>
     </div>
 </div>
--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -12,32 +12,6 @@
     </a>
 </li>
 
-<li class="nav-item {{ Request::is('sportVenues*') ? 'active' : '' }}">
-    <a class="nav-link" href="{{ route('sportVenues.index') }}">
-        <i class="nav-icon icon-cursor"></i>
-        <span>Sport Venues</span>
-    </a>
-</li>
-
-<li class="nav-item {{ Request::is('states*') ? 'active' : '' }}">
-    <a class="nav-link" href="{{ route('states.index') }}">
-        <i class="nav-icon icon-cursor"></i>
-        <span>States</span>
-    </a>
-</li>
-<li class="nav-item {{ Request::is('districts*') ? 'active' : '' }}">
-    <a class="nav-link" href="{{ route('districts.index') }}">
-        <i class="nav-icon icon-cursor"></i>
-        <span>Districts</span>
-    </a>
-</li>
-<li class="nav-item {{ Request::is('sports*') ? 'active' : '' }}">
-    <a class="nav-link" href="{{ route('sports.index') }}">
-        <i class="nav-icon icon-cursor"></i>
-        <span>Sports</span>
-    </a>
-</li>
-
 <li class="nav-item {{ Request::is('articles*') ? 'active' : '' }}">
     <a class="nav-link" href="{{ route('articles.index') }}">
         <i class="nav-icon icon-cursor"></i>
@@ -45,29 +19,10 @@
     </a>
 </li>
 
-<li class="nav-item {{ Request::is('users*') ? 'active' : '' }}">
-    <a class="nav-link" href="{{ route('users.index') }}">
-        <i class="nav-icon icon-cursor"></i>
-        <span>Users</span>
-    </a>
-</li>
-
-<li class="nav-item {{ Request::is('vendors*') ? 'active' : '' }}">
-    <a class="nav-link" href="{{ route('vendors.index') }}">
-        <i class="nav-icon icon-cursor"></i>
-        <span>Vendors</span>
-    </a>
-</li>
 <li class="nav-item {{ Request::is('feedbacks*') ? 'active' : '' }}">
     <a class="nav-link" href="{{ route('feedbacks.index') }}">
         <i class="nav-icon icon-cursor"></i>
         <span>Feedback</span>
-    </a>
-</li>
-<li class="nav-item {{ Request::is('articleTypes*') ? 'active' : '' }}">
-    <a class="nav-link" href="{{ route('articleTypes.index') }}">
-        <i class="nav-icon icon-cursor"></i>
-        <span>Article Types</span>
     </a>
 </li>
 <li class="nav-item {{ Request::is('imageSliders*') ? 'active' : '' }}">
@@ -75,3 +30,58 @@
         <i class="nav-icon icon-cursor"></i>
         <span>Image Sliders</span>
     </a>
+</li>
+<li class="nav-item nav-dropdown">
+    <a class="nav-link nav-dropdown-toggle" href="#">
+        <i class="nav-icon icon-cursor"></i>Admin Management
+    </a>
+    <ul class="nav-dropdown-items">
+        <li class="nav-item {{ Request::is('users*') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ route('users.index') }}">
+                <i class="nav-icon icon-cursor"></i>
+                <span>Users</span>
+            </a>
+        </li>
+        
+        <li class="nav-item {{ Request::is('vendors*') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ route('vendors.index') }}">
+                <i class="nav-icon icon-cursor"></i>
+                <span>Vendors</span>
+            </a>
+        </li>
+
+        <li class="nav-item {{ Request::is('sportVenues*') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ route('sportVenues.index') }}">
+                <i class="nav-icon icon-cursor"></i>
+                <span>Sport Venues</span>
+            </a>
+        </li>
+        
+        <li class="nav-item {{ Request::is('states*') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ route('states.index') }}">
+                <i class="nav-icon icon-cursor"></i>
+                <span>States</span>
+            </a>
+        </li>
+        <li class="nav-item {{ Request::is('districts*') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ route('districts.index') }}">
+                <i class="nav-icon icon-cursor"></i>
+                <span>Districts</span>
+            </a>
+        </li>
+        <li class="nav-item {{ Request::is('sports*') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ route('sports.index') }}">
+                <i class="nav-icon icon-cursor"></i>
+                <span>Sports</span>
+            </a>
+        </li>
+        <li class="nav-item {{ Request::is('articleTypes*') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ route('articleTypes.index') }}">
+                <i class="nav-icon icon-cursor"></i>
+                <span>Article Types</span>
+            </a>
+        </li>
+     
+    </ul>
+  </li>
+  
