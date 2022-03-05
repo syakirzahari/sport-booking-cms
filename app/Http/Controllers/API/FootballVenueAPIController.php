238ac56d10431f5970952f1f1298e22bff3fb694
Syakir Zahari <syakirzahari97@gmail.com>
2022-03-05 12:45:33 +0800

    update

--- app/Http/Controllers/API/FootballVenueAPIController.php
+++ app/Http/Controllers/API/FootballVenueAPIController.php
@@ -20,11 +20,17 @@ class FootballVenueAPIController extends BaseController
     {
         $data = [];
 
-        $venues = Venue::select('*')
+        $venues = Venue::select('venues.*', 'sport_venues.id AS sv_id')
                     ->join('sport_venues', 'venues.id', '=', 'sport_venues.venue_id')
                     ->where('sport_venues.sport_id', 1)
                     ->whereNull('sport_venues.deleted_at')
                     ->get();
+
+        // $venues = DB::table('venues')
+        //             ->join('sport_venues', 'venues.id', '=', 'sport_venues.venue_id')
+        //             ->where('sport_venues.sport_id', 1)
+        //             ->whereNull('sport_venues.deleted_at')
+        //             ->get();
     
         foreach ($venues as $venues) {
             $data[] = SportMenuDataPreparation::fetchSingle($venues);
